49ae7d7aa9e5497b999c4e8d46fab45bbe43f89a
Andy Barron <andybarron@protonmail.com>
2024-07-20 00:16:27 -0700

    viz, html min

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
   build: {
@@ -8,7 +9,12 @@ export default defineConfig({
     outDir: "node_modules/.vite",
     target: "esnext",
   },
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    // @ts-expect-error something's grumpy
+    ViteMinifyPlugin(),
+  ],
   assetsInclude: ["**/*.tmx", "assets/**/*.tsx"],
   resolve: {
     alias: {
